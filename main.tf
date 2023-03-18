eab67049012580bb9fea33ced3d500cb89b3ce96
Eric Liebl <32343239+maggit0@users.noreply.github.com>
2023-03-18 15:00:54 -0400

    Add files via upload
    added dependencies

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
+module "cloudfront_distribution" {
+  source = "git::https://github.com/maggit0/resume.git//modules/cloudfront_distribution"
+  depends_on = [module.s3_bucket]
+}
+
 module "s3_bucket" {
   source = "git::https://github.com/maggit0/resume.git//modules/s3_bucket"
   bucket_name = var.bucket_name
-}
-
-module "cloudfront_distribution" {
-  source = "git::https://github.com/maggit0/resume.git//modules/cloudfront_distribution"
 }
 
 module "route53_record" {
@@ -12,4 +13,5 @@ module "route53_record" {
   domain_name = var.domain_name
   zone_id = var.zone_id
   ttl = var.ttl
+  depends_on = [module.cloudfront_distribution]
 }
