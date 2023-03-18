971a55204bfbb3e982570418ef6a46f9294ae1b1
Eric Liebl <32343239+maggit0@users.noreply.github.com>
2023-03-18 14:05:31 -0400

    Add files via upload
    Removal of unnecessary variable

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 module "s3_bucket" {
-  source = ".modules/s3_bucket"
+  source = "git::https://github.com/maggit0/resume.git//modules/s3_bucket"
   bucket_name = var.bucket_name
 }
 
 module "cloudfront_distribution" {
-  source = ".modules/cloudfront_distribution"
+  source = "git::https://github.com/maggit0/resume.git//modules/cloudfront_distribution"
   domain_name = var.domain_name
   bucket_name = module.s3_bucket.bucket_id
 }
 
 module "route53_record" {
-  source = "./modules/route53_record"
+  source = "git::https://github.com/maggit0/resume.git//modules/route53_record"
   domain_name = var.domain_name
   cloudfront_url = module.cloudfront_distribution.distribution_domain_name
 }
