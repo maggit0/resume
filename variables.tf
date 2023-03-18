ca08db863a0145850160a8979207e9e42778dde1
Eric Liebl <32343239+maggit0@users.noreply.github.com>
2023-03-18 14:38:00 -0400

    Add files via upload
    updated to include new variables

--- variables.tf
+++ variables.tf
@@ -1,4 +1,25 @@
-variable "aws_region" {
-    description = "The AWS region to use"
-    default = "us-east-2"
+variable "region" {
+  description = "The AWS region"
+  type        = string
+  default = "us-east-2"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "The domain name for the Route53 record"
+  type        = string
+}
+
+variable "zone_id" {
+  description = "The ID of the Route53 hosted zone"
+  type        = string
+}
+
+variable "ttl" {
+  description = "The TTL for the Route53 record"
+  type        = number
 }
