444f32eba09027bd312e917e7368c2e094dd8333
Eric Liebl <32343239+maggit0@users.noreply.github.com>
2023-03-18 11:57:18 -0400

    Add files via upload
    Connecting to tf cloud

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  cloud {
+    organization = "ericliebl"
+    workspaces {
+      name = "learn-tfc-aws"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
